951507accac04d9fd6232cdfe359525ce0f69678
Brian Emilius <hello@brianemilius.com>
2026-04-24 20:05:21 +0200

    feat: add permission checking function for SessionUser

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
+import { SessionUser } from "@/types";
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+function can(user: SessionUser, permission: string): boolean {
+  return user.permissions.includes(permission);
 }
