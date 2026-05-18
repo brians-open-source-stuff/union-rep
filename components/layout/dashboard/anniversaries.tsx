9f6c85611ec7be816297b13a6dc460b96680c027
Brian Emilius <hello@brianemilius.com>
2026-05-18 20:46:08 +0200

    fix: update heading text for upcoming anniversaries and remove debug log

--- components/layout/dashboard/anniversaries.tsx
+++ components/layout/dashboard/anniversaries.tsx
@@ -1,10 +1,17 @@
 import { UpcomingAnniversary } from "@/data/employee-dto";
 
 export default function Anniversaries({ data }: { data: UpcomingAnniversary[] }) {
-	console.log("anniversaries", data);
 	return (
 		<div className="bg-gray-50 p-4 rounded-2xl">
-			<h2 className="mb-3 text-base font-semibold">Upcomming Anniversaries</h2>
+			<h2 className="mb-3 text-base font-semibold">Kommende jubilæer</h2>
+			<ul>
+				{data.map(employee => (
+					<li key={employee.name}>
+						<p>{employee.name} fejrer {employee.years}</p>
+						<p>{Intl.DateTimeFormat("da-DK", { dateStyle: "long" }).format(employee.employedAt)}</p>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
